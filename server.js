055864e2fdcd90c8ab8a4f64b6173b5267c3ffd5
Matt Jones <matt.mattjones@gmail.com>
2018-06-27 16:38:23 -0400

    satisfaction test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("it might be...it could be...it is!!");
+  response.end("who dosn't love a good live update");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
