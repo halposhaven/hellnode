59f793cecb245684f55cd9cf8e4d6c3697962697
Matt Jones <matt.mattjones@gmail.com>
2018-06-27 09:39:58 -0400

    buildit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello Life  Univers and everything!...42");
+  response.end("it might be...it could be...");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
