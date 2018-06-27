e566e471eb283539f9b6cd735e01b2f2f7c2c571
Matt Jones <matt.mattjones@gmail.com>
2018-06-27 09:31:03 -0400

    42

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello Life  Univers and everything!");
+  response.end("Hello Life  Univers and everything!...42");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
